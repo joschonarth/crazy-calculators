b9e5d139b88673dbf72a763035186bbf06e3e2bc
joschonarth <joschonarth@gmail.com>
2024-12-26 20:00:41 -0300

    test: add first unit test with pytest

---
+++ src/calculators/calculator_1_test.py
@@ -0,0 +1,21 @@
+from typing import Dict
+from src.calculators.calculator_1 import Calculator1
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+def test_calculate():
+    mock_request = MockRequest(body={ "number": 1 })
+    calculator_1 = Calculator1()
+
+    response = calculator_1.calculate(mock_request)
+    print()
+    print(response)
+
+    assert "data" in response
+    assert "Calculator" in response["data"]
+    assert "result" in response["data"]
+
+    assert response["data"]["result"] == 14.25
+    assert response["data"]["Calculator"] == 1
