e06fb25a1d425e0f656d644716f6e168f15cdfed
joschonarth <joschonarth@gmail.com>
2024-12-28 18:51:29 -0300

    test: add tests for handling dependencies

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,11 +1,17 @@
-from typing import Dict
+from typing import Dict, List
 from src.calculators.calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers: List[float]) -> float:
+        return 3
+
+def test_calculate_integration():
     mock_request = MockRequest({ "numbers": [2.12, 4.62, 1.32] })
     
     driver = NumpyHandler()
@@ -14,3 +20,13 @@ def test_calculate():
 
     assert isinstance(formated_response, dict)
     assert formated_response == {'data': {'Calculator': 2, 'result': 0.08}}
+
+def test_calculate():
+    mock_request = MockRequest({ "numbers": [2.12, 4.62, 1.32] })
+    
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    formated_response = calculator_2.calculate(mock_request)
+
+    assert isinstance(formated_response, dict)
+    assert formated_response == {'data': {'Calculator': 2, 'result': 0.33}}
